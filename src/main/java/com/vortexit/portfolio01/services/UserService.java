28b943e2c16e439957e7eb678cb2983e1326463c
Arlindo Chirico Neto <achiriconeto.jobs@gmail.com>
2024-01-20 20:16:38 -0300

    Service layer, component registration

---
+++ src/main/java/com/vortexit/portfolio01/services/UserService.java
@@ -0,0 +1,27 @@
+package com.vortexit.portfolio01.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vortexit.portfolio01.entities.User;
+import com.vortexit.portfolio01.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
