5b59d6f60a68c6b7b809405768eb3d087c1bb9ac
Arlindo Chirico Neto <achiriconeto.jobs@gmail.com>
2024-01-21 16:10:38 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/vortexit/portfolio01/entities/User.java
+++ src/main/java/com/vortexit/portfolio01/entities/User.java
@@ -1,13 +1,15 @@
 package com.vortexit.portfolio01.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
-
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +25,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client") //Opcional. client é o atributo obrigatório wm M:1
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -76,6 +81,10 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -92,6 +101,8 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
 
 	
 	
